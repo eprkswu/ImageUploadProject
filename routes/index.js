27764f396ec687188ed69fa139734791dc9b2bdb
eprkswu-macbook <eprkswu-macbook@192.168.14.85>
2016-01-29 17:26:24 +0900

    fdsafasdf

--- routes/index.js
+++ routes/index.js
@@ -154,7 +154,7 @@ var create_thumbnail = function(object, parent_callback){
 					code:200,
 					message:'success',
 					image_thumbnail_name:image.name,
-					image_thumbnail_path:'http://bettyvelvet.me:3001/images/thumbnail/' + new_file_name
+					image_thumbnail_path:'/images/thumbnail/' + new_file_name
 				};
 				
 				object.thumbnail_result.push(return_object);
@@ -175,56 +175,5 @@ var create_thumbnail = function(object, parent_callback){
 		parent_callback(null, object);
 	});
 };
-
-/*
-var create_thumbnail = function(original_image_path, callback){
-	var thumbnail_max_width = [130, 200, 300];
-	
-	var return_object = {};
-	
-	easyimg.info(original_image_path).then(
-		function(file){
-			for(var i in thumbnail_max_width){
-				var thumbnail_max_height = thumbnail_max_width[i] * (file.height / file.width);
-				var file_name_split = file.name.split('.');
-				var original_file_name = file_name_split[0];
-				var file_ext = file_name_split[1];
-				var new_file_name = original_file_name + '_' + thumbnail_max_width[i] + '-thumbnail.' + file_ext;
-				
-				easyimg.thumbnail({
-					src:original_image_path,
-					dst:path.join(appRoot.path,'/public/images/thumbnail/' + new_file_name),
-					width:thumbnail_max_width[i],
-					height:thumbnail_max_height
-				}).then(
-					function(image){
-						return_object = {
-							code:200,
-							message:image.name,
-							image_path:'http://bettyvelvet.me:3001/images/thumbnail/' + new_file_name
-						};
-						
-						return return_object;
-					},function(err){
-						return_object = {
-							code:500,
-							message:err.message
-						};
-						
-						return return_object;
-					}
-				);
-			}
-		},function(err){
-			return_object = {
-				code:500,
-				message:err.message
-			};
-			
-			return return_object;
-		}
-	);
-};
-*/
 
 module.exports = router;
