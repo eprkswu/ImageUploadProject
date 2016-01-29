d2e35c1c645a87bef46826a09f43cd0533c28ae4
eprkswu-macbook <eprkswu-macbook@192.168.14.85>
2016-01-29 09:25:20 +0900

    fdasfasfd

--- routes/index.js
+++ routes/index.js
@@ -19,23 +19,33 @@ router.post('/imageUpload', multer({
 	
 	for(var i in req.files){
 		var new_file_path = path.join(appRoot.path,'/public/images/', req.files[i].originalname);
-		
-		fs.rename(req.files[i].path, new_file_path, function(err){
-			if(err){
-				return_object = {
-					code:500,
-					message:err.message
-				};
-				
-				res.json(return_object);
-			}else{
-				return_object = create_thumbnail(new_file_path);
-				
-				res.json(return_object);
-			}
-		});
+		return_object = rename_file(new_file_path);
+		if(return_object.code == 200){
+			return_object = create_thumbnail(new_file_path);
+		}else{
+			res.json(return_object);
+		}
 	}
+	
+	res.json(return_object);
 });
+
+var rename_file = function(new_file_path){
+	var return_object = {};
+	
+	fs.rename(req.files[i].path, new_file_path, function(err){
+		if(err){
+			return_object = {
+				code:500,
+				message:err.message
+			};
+			
+			return return_object;
+		}else{
+			return return_object;
+		}
+	});
+};
 
 var create_thumbnail = function(original_image_path){
 	var thumbnail_max_width = [130, 200, 300];
@@ -45,8 +55,10 @@ var create_thumbnail = function(original_image_path){
 	easyimg.info(original_image_path).then(
 		function(file){
 			for(var i in thumbnail_max_width){
+				console.log(file.height);
+				console.log(file.width);
 				var thumbnail_max_height = thumbnail_max_width[i] * (file.height / file.width);
-				
+				console.log(thumbnail_max_height);
 				var file_name_split = file.name.split('.');
 				var original_file_name = file_name_split[0];
 				var file_ext = file_name_split[1];
@@ -55,7 +67,7 @@ var create_thumbnail = function(original_image_path){
 				easyimg.thumbnail({
 					src:original_image_path,
 					dst:path.join(appRoot.path,'/public/images/thumbnail/' + new_file_name),
-					width:thumbnail_max_width,
+					width:thumbnail_max_width[i],
 					height:thumbnail_max_height
 				}).then(
 					function(image){
